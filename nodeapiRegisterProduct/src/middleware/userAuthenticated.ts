53990c889b62940483fb8cf7cdbf779a2dcc48a8
Gabriel-Donizetti <gabsdonizetti@gmail.com>
2021-11-03 21:54:19 -0300

    feat: :art: auth user
    auth user to create, update, delete product and category with jwt

---
+++ nodeapiRegisterProduct/src/middleware/userAuthenticated.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from 'express'
+import { verify } from 'jsonwebtoken'
+
+interface IPayload {
+    sub: string
+}
+
+export function userAuthenticated(req: Request, res: Response, next: NextFunction){
+    const authToken = req.headers.authorization
+
+    if(!authToken){
+        return res.status(401).json({errorCode: 'invalid token'})
+    }
+
+    //Bearer 123123123jhkjh2jh432423
+    //[0] Bearer
+    //[1] token
+    const [, token] = authToken.split(' ')
+
+    try {
+        const { sub } = verify(token, process.env.JWT_SECRET) as IPayload
+        return next()
+    } catch (error) {
+        return res.status(401).json({errorCode: 'expired token'})
+    }
+}
