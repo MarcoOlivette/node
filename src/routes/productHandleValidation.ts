7a398fac48942c605a10b745dd677e82b13d3069
Gabriel-Donizetti <gabsdonizetti@gmail.com>
2021-11-04 20:48:21 -0300

    feat: :art: Add validation
    Add validation create product with express-validator

--- src/routes/productHandleValidation.ts
+++ src/routes/productHandleValidation.ts
@@ -1,5 +1,9 @@
 import { check } from 'express-validator'
 
 export const createValidation = () => [
-    check('name').isLength({ min: 5 }),
+    check('name').isLength({ min: 3}).notEmpty(),
+    check('price').isLength({ min: 3}).notEmpty(),
+    check('category').isLength({ min: 3}).notEmpty(),
+    check('amout').isLength({ min: 1}).notEmpty()
+
 ]
